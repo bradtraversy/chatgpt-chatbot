fdeaba170088e8e90312bdf3b62b9ac0f7e1f1a0
Brad Traversy <traversymedia@gmail.com>
2023-06-28 21:45:44 -0400

    Add chat history

--- index.js
+++ index.js
@@ -6,14 +6,25 @@ async function main() {
   console.log(colors.bold.green('Welcome to the Chatbot Program!'));
   console.log(colors.bold.green('You can start chatting with the bot.'));
 
+  const chatHistory = []; // Store conversation history
+
   while (true) {
     const userInput = readlineSync.question(colors.yellow('You: '));
 
     try {
-      // Call the API with user input
+      // Construct messages by iterating over the history
+      const messages = chatHistory.map(([role, content]) => ({
+        role,
+        content,
+      }));
+
+      // Add latest user input
+      messages.push({ role: 'user', content: userInput });
+
+      // Call the API with user input & history
       const completion = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: userInput }],
+        messages: messages,
       });
 
       // Get completion text/content
@@ -25,6 +36,10 @@ async function main() {
       }
 
       console.log(colors.green('Bot: ') + completionText);
+
+      // Update history with user input and assistant response
+      chatHistory.push(['user', userInput]);
+      chatHistory.push(['assistant', completionText]);
     } catch (error) {
       console.error(colors.red(error));
     }
